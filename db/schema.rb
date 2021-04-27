f8ee74cb7162c7a9883168bffd21354945a4e276
Ray <sewellray11+git@googlemail.com>
2021-04-27 11:21:38 +0100

    created relations between users and tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_094448) do
+ActiveRecord::Schema.define(version: 2021_04_27_095711) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
