82f0eeec02742698009ac58d91c31859ea2b17d2
Ray <sewellray11+git@googlemail.com>
2021-04-29 11:47:53 +0100

    added model validations, continued styling

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
     belongs_to :user
+
+    validates :body, presence: true, length: { minimum: 10, maximum: 150 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets, dependent: :destroy
+
+  validates :email, presence: true, length: { minimum: 4, maximum: 30 }
+  validates :username, presence: true, length: { minimum: 1, maximum: 16 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 22 }
 end
